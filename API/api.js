4a0c1f4aad4c4ec20e2366b852ccc46c1b08d3ae
Owais <kowais915@gmail.com>
2022-06-25 16:02:21 +0500

    setup routes for the api

--- API/api.js
+++ API/api.js
@@ -3,20 +3,58 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cheerio = require("cheerio");
 const axios = require("axios");
-PORT = process.env.PORT || 3000;
+PORT = process.env.PORTAPI || 3000;
 
 
 //express app
 const app = express();
 
-// listening
+// database and listening
 app.listen(PORT, ()=>{
     console.log("Lsitening to request at port 3000");
 });
 
+// SCRAPING START HERE
 
-//root route
+
+
+
+
+
+
+
+
+
+// ROUTES START HERE
+
+//docs page
+
 app.get("/", (req, res)=>{
 
+    res.json({msg: "Welcome to the famnet documentation page for its API"})
+})
+
+//api route page
+app.get("/api", (req, res)=>{
+
     res.json({msg: "Welcome to the famnet API"})
+})
+
+
+
+//get all content about family planning and counselling
+app.get("/api/all", (req, res)=>{
+    res.json({msg: "Here is all the content for family planning and counseling"});
+})
+
+//get content from a source when the user enters the name of a source
+app.get("/api/:source", (req, res)=>{
+    
+    res.json({msg: `Here is the content from ${req.params.source}`})
+})
+
+
+//invalid endpoint
+app.use((req, res)=>{
+    res.status(404).json({msg: "Invalid endpoint. Please cosult the documentation page"});
 })
